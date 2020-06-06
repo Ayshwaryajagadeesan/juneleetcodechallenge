db96f9bb40deec908a006e92a450a3a9754be2a9
Arun Deepak Sampath Kumar <arunayshu@Aruns-MacBook-Pro.local>
2020-06-06 07:58:02 -0400

    Queue reconstruction by height june 6th

---
+++ june6.py
@@ -0,0 +1,10 @@
+# 1. Descending order by height
+ # 2. same hight ascending order by the number of people in front
+ people.sort(key=lambda p:(-p[0], p[1]))
+ re_people = []
+
+ # Insert into the corresponding position according to the number of people in front
+ for p in people:
+     re_people.insert(p[1], p)
+
+ return re_people
